c8662f8829e42f671981de81cb88cff101ac3d11
HumayunNaseer <humayunnaseer5@gmail.com>
2021-09-14 17:41:05 +0500

    remove debugger

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < Devise::OmniauthCallbacksController
     def google_oauth2
-      debugger
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in_and_redirect @user
     end
